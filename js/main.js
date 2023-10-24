82d9b3b271d4852ced1386bed7c0d25b244e33b2
DmVal2020 <blohala90@gmail.com>
2023-10-24 10:41:32 +0300

    немного обновили лендинг

--- js/main.js
+++ js/main.js
@@ -1,17 +1,24 @@
 let texts = ['#v1', '#v2', '#v3', '#v4', '#v5']
-const arrow = document.querySelector('#b1')
+const left = document.querySelector('#left')
+const right = document.querySelector('#right')
 let count = 0
 
-b1.addEventListener('click', function (event) {
+right.addEventListener('click', function (event) {
 	event.preventDefault()
-	if (count < texts.length - 1) {
+	if (count < texts.length - 3) {
 		count += 1
-	} else {
-		count = 0
 	}
 	show()
 })
 
+left.addEventListener('click', function (event) {
+	event.preventDefault()
+
+	if (count > 0) {
+		count -= 1
+	}
+	show()
+})
 function show() {
 	for (let elem of texts) {
 		if (!document.querySelector(elem).classList.contains('visually-hidden')) {
@@ -27,13 +34,13 @@ function show() {
 			document
 				.querySelector(texts[count + i - texts.length])
 				.classList.remove('visually-hidden')
-			document
-				.querySelector('#v')
-				.append(document.querySelector(texts[count + i - texts.length]))
+			// document
+			// 	.querySelector('#v')
+			// 	.append(document.querySelector(texts[count + i - texts.length]))
 		}
 	}
-	if (count == 0) {
-		document.querySelector('#v').prepend(document.querySelector(texts[1]))
-		document.querySelector('#v').prepend(document.querySelector(texts[0]))
-	}
+	// if (count == 0) {
+	// 	document.querySelector('#v').prepend(document.querySelector(texts[1]))
+	// 	document.querySelector('#v').prepend(document.querySelector(texts[0]))
+	// }
 }
